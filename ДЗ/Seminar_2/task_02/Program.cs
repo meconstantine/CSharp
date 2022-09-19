a8915dd0c2e3df91605647241c7490deb7385064
Constantine M <mesconstantine@gmail.com>
2022-09-19 19:32:20 +0300

    new commit

---
+++ ДЗ/Seminar_2/task_02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. 
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int value = new Random().Next(100, 1000);
+int FirstDigit = value / 100 % 10;
+int SecondDigit = value % 10;
+int result = FirstDigit * 10 + SecondDigit;
+System.Console.WriteLine($"{value} -> {result}");
---
+++ ДЗ/Seminar_2/task_03/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326792 -> 6
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+
+int value = Prompt("Введите число < ");
+if (value > 99)
+{
+    int temp = value / 1;
+    int min = 999;
+    while (temp > min)
+    {
+        temp = temp / 10;
+    }
+    int result = temp % 10;
+    System.Console.WriteLine($"{value} -> {result}");
+}
+else
+{
+    System.Console.WriteLine($"{value} -> Нет третьей цифры");
+}
